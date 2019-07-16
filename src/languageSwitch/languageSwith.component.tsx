817cdabbf31949d093c168df05fe3f961dd77a2a
Giorgio Mazzei <giorgio.mazzei@outlook.com>
2019-07-16 15:36:49 +0200

    feat: languageSwitch style

--- src/languageSwitch/languageSwith.component.tsx
+++ src/languageSwitch/languageSwith.component.tsx
@@ -1,14 +1,30 @@
 import React, { ReactElement } from 'react'
-import { Button } from 'react-native'
+import { TouchableOpacity, StyleSheet } from 'react-native'
 import { LanguageSwitchDispatchProps } from './languageSwitch.model.props'
+import { Ionicons } from '@expo/vector-icons'
 
 export default class LanguageSwitch extends React.Component<
     LanguageSwitchDispatchProps
 > {
+    private styles = StyleSheet.create({
+        button: {
+            width: 50,
+            height: 50,
+            alignItems: 'center',
+            justifyContent: 'center',
+        },
+    })
     public constructor(props: LanguageSwitchDispatchProps) {
         super(props)
     }
     public render(): ReactElement {
-        return <Button title={'switch'} onPress={this.props.switchLanguage} />
+        return (
+            <TouchableOpacity
+                onPress={this.props.switchLanguage}
+                style={this.styles.button}
+            >
+                <Ionicons name="md-repeat" size={50} color={'green'} />
+            </TouchableOpacity>
+        )
     }
 }
