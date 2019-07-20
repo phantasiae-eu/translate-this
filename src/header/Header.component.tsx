4151c5f422e3b3e4f73ac37e32b874cda20b7215
Giorgio Mazzei <giorgio.mazzei@outlook.com>
2019-07-20 11:59:40 +0200

    refactor: Header component to functional component

--- src/header/Header.component.tsx
+++ src/header/Header.component.tsx
@@ -1,12 +1,10 @@
 import React, { ReactElement } from 'react'
 import { View, Text } from 'react-native'
 
-export default class Header extends React.Component {
-    public render(): ReactElement {
-        return (
-            <View>
-                <Text>Translate This</Text>
-            </View>
-        )
-    }
-}
+const header: React.FC = (): ReactElement => (
+    <View>
+        <Text>Translate This</Text>
+    </View>
+)
+
+export default header
