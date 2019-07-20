88e1ac5a4a81af257165a16d0c3caeaab0bd87f1
Giorgio Mazzei <giorgio.mazzei@outlook.com>
2019-07-20 23:16:30 +0200

    refactor: languageSwitch component from class to function

--- src/languageSwitch/languageSwith.component.tsx
+++ src/languageSwitch/languageSwith.component.tsx
@@ -3,28 +3,21 @@ import { TouchableOpacity, StyleSheet } from 'react-native'
 import { LanguageSwitchDispatchProps } from './languageSwitch.model.props'
 import { Ionicons } from '@expo/vector-icons'
 
-export default class LanguageSwitch extends React.Component<
-    LanguageSwitchDispatchProps
-> {
-    private styles = StyleSheet.create({
-        button: {
-            width: 50,
-            height: 50,
-            alignItems: 'center',
-            justifyContent: 'center',
-        },
-    })
-    public constructor(props: LanguageSwitchDispatchProps) {
-        super(props)
-    }
-    public render(): ReactElement {
-        return (
-            <TouchableOpacity
-                onPress={this.props.switchLanguage}
-                style={this.styles.button}
-            >
-                <Ionicons name="md-repeat" size={50} color={'green'} />
-            </TouchableOpacity>
-        )
-    }
-}
+const styles = StyleSheet.create({
+    button: {
+        width: 50,
+        height: 50,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+})
+
+const languageSwitch: React.FC<LanguageSwitchDispatchProps> = (
+    props: LanguageSwitchDispatchProps
+): ReactElement => (
+    <TouchableOpacity onPress={props.switchLanguage} style={styles.button}>
+        <Ionicons name="md-repeat" size={50} color={'green'} />
+    </TouchableOpacity>
+)
+
+export default languageSwitch
