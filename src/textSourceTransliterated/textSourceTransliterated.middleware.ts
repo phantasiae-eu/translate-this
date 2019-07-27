07ed3a13f59f547fe789daba1c34ab9967ba3b85
Giorgio Mazzei <giorgio.mazzei@outlook.com>
2019-07-27 15:26:50 +0200

    refactor: textSourceTransliterated.middleware

--- src/textSourceTransliterated/textSourceTransliterated.middleware.ts
+++ src/textSourceTransliterated/textSourceTransliterated.middleware.ts
@@ -3,18 +3,8 @@ import { AppState } from '../store/store.model'
 import {
     CHANGE_TEXT_SOURCE,
     AChangeTextSource,
+    textSourceTransliterate,
 } from '../textSource/textSource.actions'
-import axios, { AxiosRequestConfig, AxiosResponse } from 'axios'
-import { key } from '../../secretConfig'
-import { baseURL, apiVersion } from '../../config'
-import { transliterationOptions } from '../helpers/axios'
-import { Transliteration } from '../transliteration/transliteration.model'
-import { defaultTextTargetTransliterated } from '../textTargetTransliterated/textTargetTransliterated.model'
-import {
-    textSourceTransliteratedAccepted,
-    ATextSourceTransliteratedAccepted,
-    textSourceTransliteratedRejected,
-} from './textSourceTransliterated.actions'
 import {
     ALanguageSwitch,
     LANGUAGE_SWITCH,
@@ -36,93 +26,22 @@ export const textSourceTransliteratedMiddleware: Middleware<{}, AppState> = (
     const result = next(action)
     switch (action.type) {
         case CHANGE_TEXT_SOURCE: {
-            try {
-                const selectedLanguage: Transliteration[] = store
-                    .getState()
-                    .transliteration.filter(
-                        (language): boolean =>
-                            language.code ===
-                            store
-                                .getState()
-                                .languages.source.filter(
-                                    (language): boolean => language.selected
-                                )[0].code
-                    )
-                if (selectedLanguage.length === 0) {
-                    store.dispatch(
-                        textSourceTransliteratedAccepted(
-                            defaultTextTargetTransliterated.text
-                        )
-                    )
-                } else {
-                    const options: AxiosRequestConfig = transliterationOptions(
-                        baseURL,
-                        apiVersion,
-                        key,
-                        selectedLanguage[0].code,
-                        selectedLanguage[0].scripts[0].code,
-                        'Latn',
-                        action.text
-                    )
-                    await axios(options).then(
-                        (
-                            res: AxiosResponse
-                        ): ATextSourceTransliteratedAccepted =>
-                            store.dispatch(
-                                textSourceTransliteratedAccepted(
-                                    res.data.shift().text
-                                )
-                            )
-                    )
-                }
-            } catch (e) {
-                store.dispatch(textSourceTransliteratedRejected(e))
-            }
+            const to: string = store
+                .getState()
+                .languages.source.filter(
+                    (language): boolean => language.selected
+                )[0].code
+            store.dispatch(textSourceTransliterate(action.text, to))
             break
         }
         case LANGUAGE_SWITCH: {
-            try {
-                const selectedLanguage: Transliteration[] = store
-                    .getState()
-                    .transliteration.filter(
-                        (language): boolean =>
-                            language.code ===
-                            store
-                                .getState()
-                                .languages.target.filter(
-                                    (language): boolean => language.selected
-                                )[0].code
-                    )
-                if (selectedLanguage.length === 0) {
-                    store.dispatch(
-                        textSourceTransliteratedAccepted(
-                            defaultTextTargetTransliterated.text
-                        )
-                    )
-                } else {
-                    const options: AxiosRequestConfig = transliterationOptions(
-                        baseURL,
-                        apiVersion,
-                        key,
-                        selectedLanguage[0].code,
-                        selectedLanguage[0].scripts[0].code,
-                        'Latn',
-                        store.getState().textTarget.text
-                    )
-                    await axios(options).then(
-                        (
-                            res: AxiosResponse
-                        ): ATextSourceTransliteratedAccepted =>
-                            store.dispatch(
-                                textSourceTransliteratedAccepted(
-                                    res.data.shift().text
-                                )
-                            )
-                    )
-                }
-            } catch (e) {
-                store.dispatch(textSourceTransliteratedRejected(e))
-            }
+            const to: string = store
+                .getState()
+                .languages.target.filter(
+                    (language): boolean => language.selected
+                )[0].code
+            const text: string = store.getState().textTarget.text
+            store.dispatch(textSourceTransliterate(text, to))
         }
         default:
             break
